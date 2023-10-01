be3c7cdd95a79ca9ae3f0e1e1160f22795761fdd
tandt <daotrongtan17072003@gmail.com>
2023-10-01 21:53:53 +0700

    Service by tandt: Adress, CartItem, Color, Category

---
+++ BanMoHinh.API/IServices/IColorService.cs
@@ -0,0 +1,17 @@
+﻿using BanMoHinh.Share.Models;
+
+namespace BanMoHinh.API.IServices
+{
+    public interface IColorService
+    {
+        public Task<bool> Create(Colors item);
+
+        public Task<bool> Delete(Guid id);
+
+        public Task<List<Colors>> GetAll();
+
+        public Task<Colors> GetItem(Guid id);
+
+        public Task<bool> Update(Guid id, Colors item);
+    }
+}
