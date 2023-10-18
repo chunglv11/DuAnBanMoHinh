300f9f828926406c8a5e9eebce319459265fc76d
vanhung1103 <105692218+vanhung1103@users.noreply.github.com>
2023-10-18 18:10:27 +0700

    hung

---
+++ BanMoHinh.Client/Controllers/OrderController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BanMoHinh.Client.Controllers
+{
+    public class OrderController : Controller
+    {
+        public OrderController() { 
+
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ BanMoHinh.Client/IServices/IRateServices.cs
@@ -0,0 +1,18 @@
+﻿using BanMoHinh.Share.Models;
+
+namespace BanMoHinh.Client.IServices
+{
+    public interface IRateServices
+    {
+        public Task<bool> Create(Rate item);
+
+        public Task<bool> Delete(Guid id,Guid orderid);
+
+        public Task<List<Rate>> GetAll();
+
+        public Task<Rate> GetItem(Guid id);
+        public Task<List<Rate>> GetListRatebyorderId(Guid orderId);
+
+        public Task<bool> Update(Guid id,Guid orderid, Rate rate);
+    }
+}
