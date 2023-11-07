112e7c2611eaab32aca344491c0980543dfba3c9
tocch <tocch@TAN170703>
2023-11-07 10:31:37 +0700

    tandt

---
+++ BanMoHinh.Client/Controllers/CartController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BanMoHinh.Client.Controllers
+{
+    public class CartController : Controller
+    {
+        public async Task<IActionResult> Cart()
+        {
+            return View();
+        }
+    }
+}
