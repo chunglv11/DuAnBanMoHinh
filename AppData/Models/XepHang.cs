611e396c18ab2f7628d5375fcdb7fb0ede801b7d
chunglv11 <info.chungnd@gmail.com>
2024-01-04 13:09:26 +0700

    test All

---
+++ AppData/Models/XepHang.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppData.Models
+{
+    public class XepHang
+    {
+        public Guid Id { get; set; }
+        public string? Ten { get; set; }
+        public int? TienMin { get; set; }
+        public int? TienMax { get; set; }
+        public string? Mota { get; set; }
+        public virtual List<KhachHang>? KhachHangs { get; set; }
+    }
+}
