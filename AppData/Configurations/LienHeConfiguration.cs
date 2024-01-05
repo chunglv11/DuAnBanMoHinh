2a298efa32ab53d1f15fe3987e03b8f02876699b
chunglv11 <info.chungnd@gmail.com>
2024-01-06 00:32:15 +0700

    thêm liên hệ

---
+++ AppData/Configurations/LienHeConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using AppData.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppData.Configurations
+{
+    public class LienHeConfiguration : IEntityTypeConfiguration<LienHe>
+    {
+        public void Configure(EntityTypeBuilder<LienHe> builder)
+        {
+            builder.ToTable("LienHe");
+            builder.HasKey(x => x.Id);
+        }
+    }
+}
---
+++ AppData/Models/LienHe.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppData.Models
+{
+    public class LienHe
+    {
+        public Guid Id { get; set; }
+
+        public string HoTen { get; set; }
+
+        public string Email { get; set; }
+
+        public DateTime NgayGui { get; set; }
+
+        public string? NoiDung { get; set; }
+
+        public string? Sdt { get; set; }
+    }
+}
