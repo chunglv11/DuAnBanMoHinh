fbb0d16e3d3aa8bf975577a89ea45d14eb1ba8c7
chunglv11 <info.chungnd@gmail.com>
2024-01-08 13:55:18 +0700

    oh shit

---
+++ BanMoHinh.Client/Program.cs
@@ -0,0 +1,62 @@
+﻿using BanMoHinh.Client.IServices;
+using BanMoHinh.Client.Services;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+// add Dependency Injection
+
+builder.Services.AddHttpClient();
+builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
+builder.Services.AddScoped<IproductDetailApiClient, ProductDetailApiClient>();
+
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromSeconds(10);
+});
+// add authentication
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+   .AddCookie(options =>
+   {
+       options.LoginPath = "/Authentication/Login";
+       options.LogoutPath = "/Authentication/LogOut";
+       options.Cookie.HttpOnly = true;
+       options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+       options.Cookie.SameSite = SameSiteMode.None;
+       options.Cookie.Name = "Cookie_Cua_Trung";
+   });
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication(); // Kích hoạt xác thực
+app.UseAuthorization(); // Kích hoạt quyền truy cập
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+      name: "areas",
+      pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+    );
+    endpoints.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Authentication}/{action=Login}/{id?}");
+});
+
+app.Run();
---
+++ BanMoHinh.Share/ViewModels/ProductDetailVM.cs
@@ -0,0 +1,32 @@
+﻿using BanMoHinh.Share.Models;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BanMoHinh.Share.ViewModels
+{
+    public class ProductDetailVM
+    {
+        public Guid? Id { get; set; }
+        public Guid? ProductId { get; set; }
+        public string? ProductName { get; set; }
+        public Guid? SizeId { get; set; }
+        public string? SizeName { get; set; }
+        public double? Width { get; set; }
+        public double? Height { get; set; }
+        public Guid? ColorId { get; set; }
+        public string? ColorName { get; set; }
+        public int? Quantity { get; set; }
+        public decimal? Price { get; set; }
+        public decimal? PriceSale { get; set; }
+        public DateTime? Create_At { get; set; }
+        public DateTime? Update_At { get; set; }
+        public string? Description { get; set; }
+        public bool? Status { get; set; }
+        public List<string>? Images { get; set; } //lay link anh cua tbProImage
+        public List<IFormFile>? filecollection { get; set; }
+    }
+}
