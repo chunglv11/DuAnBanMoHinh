23c8f556d4b2f20666ec849af72dd77b3bcca714
trung truong <trung27012003@gmail.com>
2024-01-10 23:23:04 +0700

    fix add to cart

--- BanMoHinh.Client/Controllers/CartController.cs
+++ BanMoHinh.Client/Controllers/CartController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
 using System.Net.Http.Json;
 using System.Security.Claims;
+using static System.Net.WebRequestMethods;
 
 namespace BanMoHinh.Client.Controllers
 {
@@ -52,70 +53,50 @@ namespace BanMoHinh.Client.Controllers
             var identity = HttpContext.User.Identity as ClaimsIdentity;
             if (identity != null)
             {
-                var userIdClaim = identity.FindFirst(ClaimTypes.Name);
-                if (userIdClaim != null)
-                {
-                    var userName = userIdClaim.Value;
-                    var getUserbyName = await _httpClient.GetFromJsonAsync<User>($"https://localhost:7007/api/users/get/{userName}");
-                    var getallProductdetail = await _httpClient.GetFromJsonAsync<List<ProductDetailVM>>("https://localhost:7007/api/productDetail/get-all-productdetail");
-                    var getProductaddcart = getallProductdetail.Find(c => c.ProductName == ProductName && c.ColorId == colorId && c.SizeId == sizeId);
-                    var getCart = await _httpClient.GetFromJsonAsync<Cart>($"https://localhost:7007/api/cart/get-item-Cart?userId={getUserbyName.Id}");
-                    List<CartItem> getAllCartItem = await _httpClient.GetFromJsonAsync<List<CartItem>>("https://localhost:7007/api/cartitem/Get-All-CartItem");
-                    IEnumerable<CartItem> CartItembyidCart = getAllCartItem.Where(c => c.ProductDetail_ID == getProductaddcart.Id);
-
-                    var ProductInCart = CartItembyidCart.FirstOrDefault(c => c.ProductDetail_ID == getProductaddcart.Id);
-                    if (ProductInCart != null)
+                    var userID = Guid.Parse(identity.FindFirst(ClaimTypes.NameIdentifier).Value); // userId
+                    var MyCart = await _httpClient.GetFromJsonAsync<Cart>($"https://localhost:7007/api/cart/get-item-Cart?userId={userID}");// get my cart                 
+                    var ItemInMyCart = await _httpClient.GetFromJsonAsync<List<CartItem>>($"https://localhost:7007/api/cartitem/getcartitembycartid?cartid={MyCart.Id}"); // lấy item in cart
+                    var prodctDetailViewModel = await _httpClient.GetFromJsonAsync<List<ProductDetailVM>>("https://localhost:7007/api/productDetail/get-all-productdetail"); // get productdetail model
+                    var ProductDetailToAddCart = prodctDetailViewModel.FirstOrDefault(c => c.ProductName == ProductName && c.ColorId == colorId && c.SizeId == sizeId);
+                    // lấy được product detail để add cart, lấy được cart item
+                    // check exist trong cart item
+                    var checkExistInCartItem = ItemInMyCart.Where(c => c.ProductDetail_ID == ProductDetailToAddCart.Id);
+                    if (checkExistInCartItem.Count()!=1) // nếu sp không tồn tại trong cart item
                     {
-                        // cong them so luong san pham 
-                        ProductInCart.Quantity += quantity;
-                        var updateResponse = await _httpClient.PutAsJsonAsync($"https://localhost:7007/api/cartitem/Update-CartItem?id={ProductInCart.Id}", ProductInCart);
-                        return RedirectToAction("ShowCart", "Cart");
-                    }
-                    else
-                    {
-                        if (getUserbyName != null)
+                        var cartItem = new CartItem()
                         {
-                            // creat Cartdetail
-                            CartItem cartItem = new CartItem();
-                            cartItem.ProductDetail_ID = getProductaddcart.Id;
-                            cartItem.Id = Guid.NewGuid();
-                            cartItem.CartId = getCart.Id;
-                            cartItem.Price = (int?)getProductaddcart.PriceSale;
-                            if (getProductaddcart.Quantity >= quantity)
-                            {
-                                cartItem.Quantity = quantity;
-
-                            }
-                            else
-                            {// load lai trang
-                                Console.WriteLine("so luong san pham het r ban oi");
-                                return RedirectToAction("ProductDetail", "Product", new { id = getProductaddcart.Id });
-                            }
-
-                            HttpResponseMessage response = await _httpClient.PostAsJsonAsync("https://localhost:7007/api/cartitem/Insert-Cart-Item", cartItem);
-                            if (response.IsSuccessStatusCode)
-                            {
-                                Console.WriteLine("Đã tạo CartItem thành công.");
-                                // return ve gio hang 
-                                return RedirectToAction("ShowCart", "Cart");
-                            }
-                            else
-                            {
-                                Console.WriteLine("Lỗi trong việc tạo CartItem. Mã trạng thái: " + response.StatusCode);
-                                return RedirectToAction("Error");
-                            }
+                            ProductDetail_ID = ProductDetailToAddCart.Id,
+                            CartId = MyCart.Id,
+                            Price = (int)(ProductDetailToAddCart.PriceSale),
+                        };
+                        if (ProductDetailToAddCart.Quantity<quantity)
+                        {
+                            return BadRequest("Số lượng k đủ trong kho");
                         }
                         else
                         {
-                            // ban chua dang nhap cho em no ra cho dang nhao anh oi
-                            return RedirectToAction("Login", "Authentication");
+                            cartItem.Quantity = quantity;
+                            var response = await _httpClient.PostAsJsonAsync("https://localhost:7007/api/cartitem/Insert-Cart-Item", cartItem);
+                            if (!response.IsSuccessStatusCode)
+                            {
+                                return BadRequest("Lỗi Post sp ");
+                            }
+                            return RedirectToAction("ShowCart", "Cart");
                         }
+
                     }
-                }
-                else
-                { // chua biet
-                    return RedirectToAction("Login", "Authentication");
-                }
+                    else
+                    {
+                        var productDetailInCart =  checkExistInCartItem.FirstOrDefault();
+                        productDetailInCart.Quantity += quantity;
+                        var updateResponse = await _httpClient.PutAsJsonAsync($"https://localhost:7007/api/cartitem/Update-CartItem?id={productDetailInCart.Id}", productDetailInCart);
+                        if (!updateResponse.IsSuccessStatusCode)
+                        {
+                            return BadRequest("Lỗi Post sp ");
+                        }
+                        return RedirectToAction("ShowCart", "Cart");
+                    }
+
             }
             else
             {
@@ -123,85 +104,6 @@ namespace BanMoHinh.Client.Controllers
                 return RedirectToAction("Login", "Authentication");
             }
         }
-        //public async Task<IActionResult> AddtoCart(Guid prDetailId, int quantity, int Price)
-        //{
-        //    //prDetailId = Guid.Parse("51d38d0f-686e-49bc-9005-7779a7c47200");
-        //    //quantity = 1;
-        //    //Price = 123;
-        //    var identity = HttpContext.User.Identity as ClaimsIdentity;
-        //    if (identity != null)
-        //    {
-        //        var userIdClaim = identity.FindFirst(ClaimTypes.Name);
-        //        if (userIdClaim != null)
-        //        {
-        //            var userName = userIdClaim.Value;
-        //            var getUserbyName = await _httpClient.GetFromJsonAsync<User>($"https://localhost:7007/api/users/get/{userName}");
-        //            var getProductdetailbyID = await _httpClient.GetFromJsonAsync<ProductDetailVM>($"https://localhost:7007/api/productDetail/get/{prDetailId}");
-
-        //            var getCart = await _httpClient.GetFromJsonAsync<Cart>($"https://localhost:7007/api/cart/get-item-Cart?userId={getUserbyName.Id}");
-        //            List<CartItem> getAllCartItem = await _httpClient.GetFromJsonAsync<List<CartItem>>("https://localhost:7007/api/cartitem/Get-All-CartItem");
-        //            IEnumerable<CartItem> CartItembyidCart = getAllCartItem.Where(c => c.ProductDetail_ID == prDetailId);
-
-        //            var ProductInCart = CartItembyidCart.FirstOrDefault(c => c.ProductDetail_ID == prDetailId);
-        //            if (ProductInCart != null)
-        //            {
-        //                // cong them so luong san pham 
-        //                ProductInCart.Quantity += quantity;
-        //                var updateResponse = await _httpClient.PutAsJsonAsync($"https://localhost:7007/api/cartitem/Update-CartItem?id={ProductInCart.Id}", ProductInCart);
-        //                return RedirectToAction("ShowCart", "Cart");
-        //            }
-        //            else
-        //            {
-        //                if (getUserbyName != null)
-        //                {
-        //                    // creat Cartdetail
-        //                    CartItem cartItem = new CartItem();
-        //                    cartItem.ProductDetail_ID = prDetailId;
-        //                    cartItem.Id = Guid.NewGuid();
-        //                    cartItem.CartId = getCart.Id;
-
-        //                    if (getProductdetailbyID.Quantity >= quantity)
-        //                    {
-        //                        cartItem.Quantity = quantity;
-
-        //                    }
-        //                    else
-        //                    {// load lai trang
-        //                        Console.WriteLine("so luong san pham het r ban oi");
-        //                        return RedirectToAction("ProductDetail", "Product", new { id = prDetailId });
-        //                    }
-        //                    cartItem.Price = Price;
-        //                    HttpResponseMessage response = await _httpClient.PostAsJsonAsync("https://localhost:7007/api/cartitem/Insert-Cart-Item", cartItem);
-        //                    if (response.IsSuccessStatusCode)
-        //                    {
-        //                        Console.WriteLine("Đã tạo CartItem thành công.");
-        //                        // return ve gio hang 
-        //                        return RedirectToAction("ShowCart", "Cart");
-        //                    }
-        //                    else
-        //                    {
-        //                        Console.WriteLine("Lỗi trong việc tạo CartItem. Mã trạng thái: " + response.StatusCode);
-        //                        return RedirectToAction("Error");
-        //                    }
-        //                }
-        //                else
-        //                {
-        //                    // ban chua dang nhap cho em no ra cho dang nhao anh oi
-        //                    return RedirectToAction("Login", "Authentication");
-        //                }
-        //            }
-        //        }
-        //        else
-        //        { // chua biet
-        //            return RedirectToAction("Login", "Authentication");
-        //        }
-        //    }
-        //    else
-        //    {
-        //        // ban chua dang nhap cho em no ra cho dang nhao anh oi
-        //        return RedirectToAction("Login", "Authentication");
-        //    }
-        //}
         public async Task<IActionResult> TangSL(Guid id, Guid idCartItem, Guid idgh)
         {
             try
